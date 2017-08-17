3b0fc94bb7c775f0d9d9856e57ecda463e87b4b3
YOUR NAME <YOUR@EMAIL.com>
2017-08-18 00:25:14 +0100

    add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates_confirmation_of :password
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :salt, :encrypted_password)
+  end
 end
