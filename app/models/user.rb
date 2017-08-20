15595c3d480dead8614b0cd9c2e69b26d02fa6e1
YOUR NAME <YOUR@EMAIL.com>
2017-08-20 16:58:58 +0100

    update user registration page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  validates_confirmation_of :password
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { in: 4..20 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
   def create
     @user = User.new(user_params)
   end
@@ -7,6 +11,6 @@ class User < ApplicationRecord
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :salt, :encrypted_password)
+    params.require(:user).permit(:name, :email, :password, :salt, :encrypted_password)
   end
 end
