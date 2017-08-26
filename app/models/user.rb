f5084db5d5a7cc08267e5f66422fa93dbec149ae
YOUR NAME <YOUR@EMAIL.com>
2017-08-26 20:54:14 +0100

    add avatar to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
-  validates :name, :email, :password, presence: true
+  # This method associates the attribute ":avatar" with a file attachment
+  has_attached_file :avatar, styles: {
+    square: '200x200#'
+  }
+
+  validates :name, :email, :password, :avatar, presence: true
   validates :email, uniqueness: true
   validates :password, length: { in: 4..20 }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   def create
     @user = User.new(user_params)
   end
