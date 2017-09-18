afcc1451cbe7cb1b187ecb0ea74972401879a842
YOUR NAME <YOUR@EMAIL.com>
2017-09-18 21:28:25 +0100

    add interests to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817214630) do
+ActiveRecord::Schema.define(version: 20170918184947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170817214630) do
     t.string "avatar_url", limit: 1024
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "interests"
   end
 
 end
