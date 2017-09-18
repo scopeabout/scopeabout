d17213efb1842103fbbf397b8726fefee17e43b9
YOUR NAME <YOUR@EMAIL.com>
2017-09-18 21:28:25 +0100

    add interests to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,11 @@ class User < ApplicationRecord
   validates :password, length: { in: 4..20 }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
+
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  validates :interests, presence: true
+  validates_length_of :interests_word_count, minimum: 3, maximum: 10, too_short: "should at least be %{count} words", too_long: "should not be more than %{count} words"
 
   def create
     @user = User.new(user_params)
@@ -17,7 +21,11 @@ class User < ApplicationRecord
 
   private
 
+  def interests_word_count
+    interests.scan(/\w+/)
+  end
+
   def user_params
-    params.require(:user).permit(:name, :email, :password, :salt, :encrypted_password)
+    params.require(:user).permit(:name, :email, :password, :salt, :encrypted_password, :interests)
   end
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,8 @@
-json.extract! user, :id, :created_at, :updated_at
+json.extract! user,
+              :id,
+              :name,
+              :email,
+              :interests,
+              :created_at,
+              :updated_at
 json.url user_url(user, format: :json)
