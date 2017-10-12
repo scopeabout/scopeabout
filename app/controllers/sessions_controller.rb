e14ded24409985988c72fb1d3c29e4c1b2600e7f
Sinaru Gunawardena <sinarug@gmail.com>
2017-10-13 00:20:42 +0100

    update sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,15 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to root_url if logged_in?
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
-      render 'welcome_page'
+      log_in user
+      redirect_to root_url
     else
-      # Create an error message.
+      @error = 'Invalid email/password combination'
       render 'new'
     end
   end
