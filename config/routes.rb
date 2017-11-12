a63fbabc8cb4002aef85ad7ca4f4414dcee02786
Sinaru Gunawardena <sinarug@gmail.com>
2017-11-12 21:57:32 +0000

    use secure password for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'welcome_page#welcome'
   get 'welcome_page/welcome'
+  get 'sessions/new'
 
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources 'users'
   get 'register', to: 'users#new'
   resources 'users'
 end
