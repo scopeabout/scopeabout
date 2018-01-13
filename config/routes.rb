a262081e9fe5b9b16a02d359c3e717612fb80694
Sinaru Gunawardena <sinarug@gmail.com>
2018-01-13 11:57:12 +0000

    update routes paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome_page#welcome'
+  
   get 'welcome_page/welcome'
   get 'sessions/new'
-
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-
-  resources 'users'
   get 'register', to: 'users#new'
   resources 'users'
 end
