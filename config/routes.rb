7420f03567e19cd2c10a9a3cfa02c4e519f626dd
Sinaru Gunawardena <sinarug@gmail.com>
2019-03-05 21:58:58 +0000

    add routes for sessions and feed page for org user under subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  scope module: 'org' do
+    constraints OrganizationRouteConstrains.new do
+      get '/', to: 'feeds#index'
+      get '/login', to: 'user_sessions#new'
+      post '/login', to: 'user_sessions#create'
+      get 'logout', to: 'user_sessions#destroy'
+      get '/logout_success', to: 'user_sessions#logout_success'
+    end
+  end
+
   get '/push_notifications', to: 'welcome_page#push_notifications'
   get '/cookies', to: 'welcome_page#cookies_consent'
   root 'welcome_page#welcome'
